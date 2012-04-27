06560f2d475ef191a0792291c2a23c19511456c0
scott <scott@adligo.com>
2012-04-27 00:23:08 +0000

    ok this project is fairly complete, on to auto generating code to use it

--- src/org/adligo/xml_io/client/CustomComplexModelConverter.java
+++ src/org/adligo/xml_io/client/CustomComplexModelConverter.java
@@ -1,20 +1,150 @@
 package org.adligo.xml_io.client;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.adligo.i.util.client.I_Iterator;
 import org.adligo.models.params.client.I_XMLBuilder;
+import org.adligo.models.params.client.Parser;
+import org.adligo.models.params.client.TagAttribute;
 import org.adligo.models.params.client.TagInfo;
+import org.adligo.xml_io.client.converters.ClassMappings;
 import org.adligo.xml_io.client.converters.Tags;
 
 public class CustomComplexModelConverter implements I_Converter<CustomComplexModel>{
 	private static final String TAG_NAME = "ccm";
+	private static final Map<String, I_AttributeSetter<CustomComplexModel>> SETTERS = getSetters();
+	private static final Map<String, I_FieldSetter<CustomComplexModel>> FIELD_SETTERS = getFieldSetters();
 	
+	public static Map<String, I_AttributeSetter<CustomComplexModel>> getSetters() {
+		Map<String, I_AttributeSetter<CustomComplexModel>> toRet = 
+			new HashMap<String, I_AttributeSetter<CustomComplexModel>>();
+		
+		toRet.put("id", new I_AttributeSetter<CustomComplexModel>() {
+			public void set(CustomComplexModel obj, String value, Xml_IOReaderContext context) {
+				Object toSet = context.readAttribute(Integer.class, value);
+				obj.setId((Integer) toSet);
+			}
+		});
+		
+		toRet.put("sid", new I_AttributeSetter<CustomComplexModel>() {
+			public void set(CustomComplexModel obj, String value, Xml_IOReaderContext context) {
+				Object toSet = context.readAttribute(Short.class, value);
+				obj.setSid((Short) toSet);
+			}
+		});
+		
+		toRet.put("lid", new I_AttributeSetter<CustomComplexModel>() {
+			public void set(CustomComplexModel obj, String value, Xml_IOReaderContext context) {
+				Object toSet = context.readAttribute(Long.class, value);
+				obj.setLid((Long) toSet);
+			}
+		});
+		
+		toRet.put("did", new I_AttributeSetter<CustomComplexModel>() {
+			public void set(CustomComplexModel obj, String value, Xml_IOReaderContext context) {
+				Object toSet = context.readAttribute(Double.class, value);
+				obj.setDid((Double) toSet);
+			}
+		});
+		
+		toRet.put("fid", new I_AttributeSetter<CustomComplexModel>() {
+			public void set(CustomComplexModel obj, String value, Xml_IOReaderContext context) {
+				Object toSet = context.readAttribute(Float.class, value);
+				obj.setFid((Float) toSet);
+			}
+		});
+		
+		toRet.put("chars", new I_AttributeSetter<CustomComplexModel>() {
+			public void set(CustomComplexModel obj, String value, Xml_IOReaderContext context) {
+				Object toSet = context.readAttribute(ClassMappings.CHAR_ARRAY_CLASS, value);
+				obj.setChars((char []) toSet);
+			}
+		});
+		
+		toRet.put("bytes", new I_AttributeSetter<CustomComplexModel>() {
+			public void set(CustomComplexModel obj, String value, Xml_IOReaderContext context) {
+				Object toSet = context.readAttribute(ClassMappings.BYTE_ARRAY_CLASS, value);
+				obj.setBytes((byte []) toSet);
+			}
+		});
+		
+		toRet.put("bools", new I_AttributeSetter<CustomComplexModel>() {
+			public void set(CustomComplexModel obj, String value, Xml_IOReaderContext context) {
+				Object toSet = context.readAttribute(ClassMappings.BOOLEAN_ARRAY_CLASS, value);
+				obj.setBools((boolean []) toSet);
+			}
+		});
+		
+
+		return Collections.unmodifiableMap(toRet);
+	}
+	
+	
+	public static Map<String, I_FieldSetter<CustomComplexModel>> getFieldSetters() {
+		Map<String, I_FieldSetter<CustomComplexModel>> toRet = 
+			new HashMap<String, I_FieldSetter<CustomComplexModel>>();
+
+		toRet.put("stringsList", new I_FieldSetter<CustomComplexModel>() {
+			@SuppressWarnings("unchecked")
+			public void set(CustomComplexModel obj, Object value) {
+				obj.setStringsList((List<String>) value);
+			}
+		});
+		
+		toRet.put("stringsArray", new I_FieldSetter<CustomComplexModel>() {
+			@SuppressWarnings("unchecked")
+			public void set(CustomComplexModel obj, Object value) {
+				List<String> items = (List<String>) value;
+				String [] itemsArray = new String [items.size()];
+				items.toArray(itemsArray);
+				obj.setStringsArray(itemsArray);
+			}
+		});
+		
+		toRet.put("keyValues", new I_FieldSetter<CustomComplexModel>() {
+			@SuppressWarnings("unchecked")
+			public void set(CustomComplexModel obj, Object value) {
+				obj.setKeyValues((Map<Integer, String>) value);
+			}
+		});
+		
+		return Collections.unmodifiableMap(toRet);
+	}
 	@Override
 	public ObjectFromXml<CustomComplexModel> fromXml(String xml, TagInfo info,
 			Xml_IOReaderContext context) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		//the CustomModel class is mutable so just call setters
+		CustomComplexModel toRet = new CustomComplexModel();
+		I_Iterator it = Parser.getAttributes(info, xml);
+		while (it.hasNext()) {
+			TagAttribute attrib = (TagAttribute) it.next();
+			String name = attrib.getName();
+			String value = attrib.getValue();
+			
+			//don't call a setter for name
+			if ( !Tags.NAME_ATTRIBUTE.equals(name)){
+				I_AttributeSetter<CustomComplexModel> setter = SETTERS.get(name);
+				setter.set(toRet, value, context);
+			}
+		}
+		it = info.getChildren();
+		while (it.hasNext()) {
+			TagInfo child = (TagInfo) it.next();
+			String subTag = Parser.substring(xml, child);
+			ObjectFromXml<?> obj =  context.readXml(subTag);
+			String name = obj.getName();
+			I_FieldSetter<CustomComplexModel> setter = FIELD_SETTERS.get(name);
+			Object value = obj.getValue();
+			setter.set(toRet, value);
+		}
+		
+		return new ObjectFromXml<CustomComplexModel>(toRet);
 	}
 
 	@Override
--- src/org/adligo/xml_io/client/Xml_IOReaderCustomTests.java
+++ src/org/adligo/xml_io/client/Xml_IOReaderCustomTests.java
@@ -1,6 +1,8 @@
 package org.adligo.xml_io.client;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
 
 import org.adligo.models.params.client.XMLBuilder;
 import org.adligo.tests.ATest;
@@ -22,4 +24,72 @@ public class Xml_IOReaderCustomTests  extends ATest {
 		
 	}
 
+	
+	public void testComplexCustomModelToXml() {
+		Xml_IOSettings settings = new Xml_IOSettings();
+		CustomComplexModelConverter.setUp(settings);
+		
+		
+		Xml_IOReader reader = new Xml_IOReader();
+		Object obj = reader.readXml(
+				"<ccm id=\"9999\" sid=\"99\" lid=\"99999999\" did=\"99.99\" fid=\"9.9\" chars=\"ufda\" bytes=\"AAA=\" bools=\"t\">" 
+				+ XMLBuilder.DOS_LINE_FEED 
+				+ XMLBuilder.SPACE_INDENT + "<L n=\"stringsList\">" + XMLBuilder.DOS_LINE_FEED 
+				+ XMLBuilder.SPACE_INDENT + XMLBuilder.SPACE_INDENT + "<s>1ab</s>" + XMLBuilder.DOS_LINE_FEED 
+				+ XMLBuilder.SPACE_INDENT + XMLBuilder.SPACE_INDENT + "<s>1ac</s>" + XMLBuilder.DOS_LINE_FEED
+				+ XMLBuilder.SPACE_INDENT + "</L>" + XMLBuilder.DOS_LINE_FEED
+				+ XMLBuilder.SPACE_INDENT + "<L n=\"stringsArray\">"  + XMLBuilder.DOS_LINE_FEED
+				+ XMLBuilder.SPACE_INDENT + XMLBuilder.SPACE_INDENT + "<s>ba</s>"  + XMLBuilder.DOS_LINE_FEED
+				+ XMLBuilder.SPACE_INDENT + XMLBuilder.SPACE_INDENT + "<s>bb</s>"  + XMLBuilder.DOS_LINE_FEED
+		      	+ XMLBuilder.SPACE_INDENT + "</L>"  + XMLBuilder.DOS_LINE_FEED
+		      	+ XMLBuilder.SPACE_INDENT + "<m n=\"keyValues\">"  + XMLBuilder.DOS_LINE_FEED
+		      	+ XMLBuilder.SPACE_INDENT + XMLBuilder.SPACE_INDENT + "<k>"  + XMLBuilder.DOS_LINE_FEED
+		      	+ XMLBuilder.SPACE_INDENT + XMLBuilder.SPACE_INDENT + XMLBuilder.SPACE_INDENT + "<i>11</i>"  + XMLBuilder.DOS_LINE_FEED
+		      	+ XMLBuilder.SPACE_INDENT + XMLBuilder.SPACE_INDENT + XMLBuilder.SPACE_INDENT + "<s>aa</s>"  + XMLBuilder.DOS_LINE_FEED
+		        + XMLBuilder.SPACE_INDENT + XMLBuilder.SPACE_INDENT + "</k>"  + XMLBuilder.DOS_LINE_FEED
+		        + XMLBuilder.SPACE_INDENT + XMLBuilder.SPACE_INDENT + "<k>"  + XMLBuilder.DOS_LINE_FEED
+		        + XMLBuilder.SPACE_INDENT + XMLBuilder.SPACE_INDENT + XMLBuilder.SPACE_INDENT + "<i>22</i>"  + XMLBuilder.DOS_LINE_FEED
+		        + XMLBuilder.SPACE_INDENT + XMLBuilder.SPACE_INDENT + XMLBuilder.SPACE_INDENT + "<s>bb</s>"  + XMLBuilder.DOS_LINE_FEED
+		        + XMLBuilder.SPACE_INDENT + XMLBuilder.SPACE_INDENT + "</k>"  + XMLBuilder.DOS_LINE_FEED
+		      	+ XMLBuilder.SPACE_INDENT + "</m>"  + XMLBuilder.DOS_LINE_FEED
+		      	+ "</ccm>" + XMLBuilder.DOS_LINE_FEED, settings);
+		assertTrue(obj instanceof CustomComplexModel);
+		CustomComplexModel mod = (CustomComplexModel) obj;
+		assertEquals(9999, mod.getId());
+		assertEquals((short) 99, mod.getSid());
+		assertEquals(99999999L, mod.getLid());
+		assertEquals(99.99, mod.getDid());
+		assertEquals((float) 9.9, mod.getFid());
+		
+		byte [] bytes = mod.getBytes();
+		assertEquals(2, bytes.length);
+		assertEquals((byte) 0, bytes[0]);
+		assertEquals((byte) 0, bytes[1]);
+		
+		char [] chars = mod.getChars();
+		assertEquals(4, chars.length);
+		assertEquals('u', chars[0]);
+		assertEquals('f', chars[1]);
+		assertEquals('d', chars[2]);
+		assertEquals('a', chars[3]);
+		
+		boolean [] bools = mod.getBools();
+		assertEquals(1, bools.length);
+		assertTrue(bools[0]);
+		
+		String [] sa = mod.getStringsArray();
+		assertTrue(sa.length == 2);
+		assertEquals("ba", sa[0]);
+		assertEquals("bb", sa[1]);
+		
+		List<String> stringList = mod.getStringsList();
+		assertTrue(stringList.size() == 2);
+		assertEquals("1ab", stringList.get(0));
+		assertEquals("1ac", stringList.get(1));
+		
+		Map<Integer,String> kv = mod.getKeyValues();
+		assertTrue(kv.size() == 2);
+		assertEquals("aa", kv.get(11));
+		assertEquals("bb", kv.get(22));
+	}
 }
