fba033131cfe34f6284f24d7faf8cb1d33360982
scott <scott@adligo.com>
2012-09-27 23:05:14 +0000

    added real test for config

--- src/org/adligo/xml_io/client/NamespaceConvertersTests.java
+++ src/org/adligo/xml_io/client/NamespaceConvertersTests.java
@@ -1,12 +1,48 @@
 package org.adligo.xml_io.client;
 
-import org.adligo.i.util.client.ClassUtils;
+import java.util.Map;
+
 import org.adligo.tests.ATest;
 
 public class NamespaceConvertersTests extends ATest {
 
-	public void testFoo() {
-		int[] foo = new int[1];
-		System.out.println(ClassUtils.getClassName(foo.getClass()));
+	public void testGetsAndSets_ForImmutableMaps() {
+		
+		NamespaceConvertersMutant mutant = new NamespaceConvertersMutant();
+		mutant.setNamespace("ns");
+		assertEquals("ns", mutant.getNamespace());
+		
+		mutant.setPackageName("pk");
+		assertEquals("pk", mutant.getPackageName());
+		
+		Map<Class<?>, I_AttributeConverter<?>> attrCons =  mutant.getAttributeConverters();
+		Exception caught = null;
+		try {
+			attrCons.put(Exception.class, null);
+		} catch (Exception g) {
+			caught = g;
+		}
+		assertNotNull(caught);
+		assertEquals(UnsupportedOperationException.class, caught.getClass());
+		
+		Map<Class<?>, I_Converter<?>> cons =  mutant.getObjectToXmlConverters();
+		caught = null;
+		try {
+			cons.put(Exception.class, null);
+		} catch (Exception g) {
+			caught = g;
+		}
+		assertNotNull(caught);
+		assertEquals(UnsupportedOperationException.class, caught.getClass());
+		
+		Map<String, I_Converter<?>> fromXmlCons =   mutant.getXmlToObjectConverters();
+		caught = null;
+		try {
+			fromXmlCons.put("", null);
+		} catch (Exception g) {
+			caught = g;
+		}
+		assertNotNull(caught);
+		assertEquals(UnsupportedOperationException.class, caught.getClass());
 	}
 }
