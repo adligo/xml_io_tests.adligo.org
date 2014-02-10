ee5eb5d04df402ed69eea55798b3bb8d8b4cd7b3
scott <scott@adligo.com>
2014-02-10 16:15:35 -0600

    new class

---
+++ src/org/adligo/xml_io/tests/client/MockLetterCounter.java
@@ -0,0 +1,10 @@
+package org.adligo.xml_io.tests.client;
+
+import org.adligo.xml_io.client.LetterCounter;
+
+public class MockLetterCounter extends LetterCounter {
+
+	public int findDigitToChange(char [] chars) {
+		return super.findDigitToChange(chars);
+	}
+}
